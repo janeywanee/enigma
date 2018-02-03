3cf415049f5ec62268bbb59904d7d82194eae3d1
Jane Kim <Janeekim12@gmail.com>
2018-02-03 11:56:36 -0700

    hound changes and bad number test

--- test/key_test.rb
+++ test/key_test.rb
@@ -1,9 +1,7 @@
 require "./test/test_helper"
-
 require "./lib/key"
 
-class Test_Key < Minitest::Test
-
+class TestKey < Minitest::Test
   def test_key_exists
     key = Key.new
     assert_instance_of Key, key
@@ -13,7 +11,4 @@ class Test_Key < Minitest::Test
     key = Key.new
     assert_equal key.num, 12345
   end
-
-
-
 end
