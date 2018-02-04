887a506cb1c4d9ebbeb88c8ca3d6a53d2d9ed0df
Megan Arellano <ssciolist@gmail.com>
2018-02-04 12:16:39 -0700

    very ugly offsets

--- lib/offset.rb
+++ lib/offset.rb
@@ -4,11 +4,34 @@ require "date"
 class Offset
   attr_reader :today
 
+
   def initialize
     @today = Date.today
+    @a_offset = ""
   end
 
   def convert_date
     @today.strftime("%d%m%y")
+  end
+
+  def last_four
+    squared_date = convert_date.to_i ** 2
+    squared_date.to_s[-4..-1]
+  end
+
+  def a_offset
+    @a_offset = last_four[-4]
+  end
+
+  def b_offset
+    @b_offset = last_four[-3]
+  end
+
+  def c_offset
+    @c_offset = last_four[-2]
+  end
+
+  def d_offset
+    @d_offset = last_four[-1]
   end
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -11,7 +11,19 @@ class TestOffset < Minitest::Test
 
   def test_convert_date_into_ddmmyy
     offset = Offset.new
-    binding.pry
     assert_equal offset.convert_date, "040218"
+  end
+
+  def test_last_four_of_squared_date
+    offset = Offset.new
+    assert_equal offset.last_four.length, 4
+  end
+
+  def test_offset_a_thru_d
+    offset = Offset.new
+    assert_equal offset.a_offset, offset.last_four[-4]
+    assert_equal offset.b_offset, offset.last_four[-3]
+    assert_equal offset.c_offset, offset.last_four[-2]
+    assert_equal offset.d_offset, offset.last_four[-1]
   end
 end
