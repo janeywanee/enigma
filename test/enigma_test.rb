039fe741492b6ba3c53379cea06a2661e26a2155
Jane Kim <Janeekim12@gmail.com>
2018-02-06 16:42:56 -0700

    finished decrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,33 +13,26 @@ class TestEnigma < Minitest::Test
     assert_instance_of Offset, enigma.offset
   end
 
-  def test_string_separate_into_4_chars
-    enigma = Enigma.new
-    message = "this is so secret"
-    enigma.separate(message)
-    assert_equal enigma.separate(message).count, 4
-  end
-
   def test_first_character_rotates_by_a
     enigma = Enigma.new
     first_character = "t"
     assert ("a".."z").include?(enigma.rotate_a(first_character))
   end
 
-  def test_second_character_rotates_by_a
+  def test_second_character_rotates_by_b
     enigma = Enigma.new
     second_character = "h"
-binding.pry
+
     assert ("a".."z").include?(enigma.rotate_a(second_character))
   end
 
-  def test_third_character_rotates_by_a
+  def test_third_character_rotates_by_c
     enigma = Enigma.new
     third_character = "i"
     assert ("a".."z").include?(enigma.rotate_a(third_character))
   end
 
-  def test_fourth_character_rotates_by_a
+  def test_fourth_character_rotates_by_d
     enigma = Enigma.new
     fourth_character = "s"
     assert ("a".."z").include?(enigma.rotate_a(fourth_character))
@@ -62,5 +55,18 @@ binding.pry
     enigma = Enigma.new
     encryption = enigma.encrypt("this is so secret")
     assert_equal encryption.length, "thisissosecret".length
+  end
+
+  def test_decrypt_for_first_character
+    enigma = Enigma.new
+    first_character = "t"
+    assert_equal enigma.decrypt_letter_a(enigma.rotate_a(first_character)), "t"
+  end
+
+  def test_decrypt_bcd_chars
+    enigma =Enigma.new
+    assert_equal enigma.decrypt_letter_b(enigma.rotate_b("h")), "h"
+    assert_equal enigma.decrypt_letter_c(enigma.rotate_c("i")), "i"
+    assert_equal enigma.decrypt_letter_d(enigma.rotate_d("s")), "s"
   end
 end
